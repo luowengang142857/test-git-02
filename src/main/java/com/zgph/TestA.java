fb87d620845af7c94c91150e8c54a21849989b80
lwg <15983316702@163.com>
2019-09-13 10:18:52 +0800

    first commit

---
+++ src/main/java/com/zgph/TestA.java
@@ -0,0 +1,19 @@
+package com.zgph;
+
+/**
+ * @author lwg
+ * @Date: 2019/9/13 10:16
+ */
+public class TestA {
+    public static void main(String[] args) {
+
+        System.out.println("aaaa------------");
+
+        String os = "13457";
+
+        long result = Long.parseLong(os);
+
+        System.out.println(result);
+
+    }
+}
