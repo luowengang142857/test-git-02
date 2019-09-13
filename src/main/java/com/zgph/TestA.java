5ea3d2c7bdc6072e14bbdc1124f9b036e291b5da
lwg <15983316702@163.com>
2019-09-13 10:35:56 +0800

    d

--- src/main/java/com/zgph/TestA.java
+++ src/main/java/com/zgph/TestA.java
@@ -11,11 +11,21 @@ public class TestA {
 
         String os = "13457000";
 
+        String osa = "sfaf";
+
+        int a = 123;
+
 
 
         long result = Long.parseLong(os);
 
+        if(result > a){
+            System.out.println("result > a");
+        }
+
         System.out.println(result);
+
+        System.out.println("----------------end");
 
     }
 }
